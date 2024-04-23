5055208201691a5aa9d2b16e3d0f66163653f6eb
manuelo69 <ichigo2418@gmail.com>
2024-04-23 11:19:40 +0200

    first commit

---
+++ src/components/Hijo.jsx
@@ -0,0 +1,14 @@
+import { useUserToggleContext, useUserContext } from "../UserProvider";
+const Hijo = () => {
+  const user = useUserContext();
+  const cambiaLogin = useUserToggleContext();
+  return (
+    <div>
+      <h2>Componente Hijo</h2>
+      {user && <p>Hola {user.name}</p>}
+      <button onClick={cambiaLogin}>Cambia Login</button>
+    </div>
+  );
+};
+
+export default Hijo;
